cd5055b614c9be3b22f41a64cadcc729407ccb28
Amanda Ventura <74805696+ASV185@users.noreply.github.com>
2023-12-31 23:22:52 -0700

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generateBrand(event) {
-  event.preventDefault();
-
+function displayBrands(response) {
   new Typewriter("#brands", {
-    strings: "Attitude",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateBrand(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "6a8co22f6f92bdd5a654001ta38ff409";
+  let prompt = `Please generate nontoxic ${instructionsInput.value} brands.`;
+  let context =
+    "User instructions: You are a smart AI assistant please name a nontoxic product brand. Make sure to follow user instructions. The brands must be provided in HTML format. Example: <p>this is a brand</p>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayBrands);
 }
 
 let brandFormElement = document.querySelector("#nontoxic-brand-generator-form");
